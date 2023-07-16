14291e23e5fade87b2f9ed409bd214bf8065a8ce
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-16 20:12:15 +0300

    Create splash.dart

---
+++ ruqayyah/lib/src/views/splash.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:ruqayyah/src/views/app.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) {
+            return const App();
+          },
+        ),
+      );
+    });
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          scale: 3,
+        ),
+      ),
+    );
+  }
+}
