117c9fe18891e52d9722b8d348c9c6e7a93fde2c
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-16 17:20:00 +0300

    Update services.dart

--- ruqayyah/lib/services.dart
+++ ruqayyah/lib/services.dart
@@ -1,1 +1,12 @@
-Future initServices() async {}
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+Future initServices() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+}
