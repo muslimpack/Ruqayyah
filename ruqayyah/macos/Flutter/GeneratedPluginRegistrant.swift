9bd0bf36415e4fb7552502eb4b70e6feaeabe0a0
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-16 15:29:14 +0300

    Add Packages

--- ruqayyah/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ruqayyah/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import device_info_plus
+import path_provider_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
