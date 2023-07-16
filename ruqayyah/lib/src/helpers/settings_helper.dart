e067b9302e0f28a724971732135c16f6da95ca8e
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-17 02:06:58 +0300

    Update settings_helper.dart

--- ruqayyah/lib/src/helpers/settings_helper.dart
+++ ruqayyah/lib/src/helpers/settings_helper.dart
@@ -16,33 +16,19 @@ class SettingsHelper {
   /// Sounds
   ///********************
 
-  /// get Tally Sound mode
   bool get isTallySoundAllowed => box.read('tally_sound') ?? false;
 
-  /// set Tally Sound mode
   void changeTallySoundStatus({required bool value}) =>
       box.write('tally_sound', value);
 
-  /// get Tally Done Sound mode
   bool get isZikrDoneSoundAllowed => box.read('zikr_done_sound') ?? false;
 
-  /// set Tally Done Sound mode
   void changeZikrDoneSoundStatus({required bool value}) =>
       box.write('zikr_done_sound', value);
 
-  /// get Tally Transition Sound mode
-  bool get isTransitionSoundAllowed =>
-      box.read('tally_transition_sound') ?? false;
-
-  /// set Tally  Transition Sound mode
-  void changeTransitionSoundStatus({required bool value}) =>
-      box.write('tally_transition_sound', value);
-
-  /// get Tally Finished Sound mode
   bool get isAllAzkarFinishedSoundAllowed =>
       box.read('all_azkar_finished_sound') ?? false;
 
-  /// set Tally Finished Sound mode
   void changeAllAzkarFinishedSoundStatus({required bool value}) =>
       box.write('all_azkar_finished_sound', value);
 
@@ -50,40 +36,24 @@ class SettingsHelper {
   /// Vibration
   ///********************
 
-  /// get Sound Effect Volume
   double get soundEffectVolume => box.read('soundEffectVolume') ?? 1;
 
-  /// set Tally Vibrate mode
   void changeSoundEffectVolume(double value) =>
       box.write('soundEffectVolume', value);
 
-  /// get Tally Vibrate mode
   bool get isTallyVibrateAllowed => box.read('tally_vibrate') ?? false;
 
-  /// set Tally Vibrate mode
   void changeTallyVibrateStatus({required bool value}) =>
       box.write('tally_vibrate', value);
 
-  /// get Tally Done Vibrate mode
   bool get isZikrDoneVibrateAllowed => box.read('zikr_done_vibrate') ?? false;
 
-  /// set Tally Done Vibrate mode
   void changeZikrDoneVibrateStatus({required bool value}) =>
       box.write('zikr_done_vibrate', value);
 
-  /// get Tally Transition Vibrate mode
-  bool get isTransitionVibrateAllowed =>
-      box.read('tally_transition_vibrate') ?? false;
-
-  /// set Tally  Transition Vibrate mode
-  void changeTransitionVibrateStatus({required bool value}) =>
-      box.write('tally_transition_vibrate', value);
-
-  /// get Tally Finished Vibrate mode
   bool get isAllAzkarFinishedVibrateAllowed =>
       box.read('all_azkar_finished_vibrate') ?? false;
 
-  /// set Tally Finished Vibrate mode
   void changeAllAzkarFinishedVibrateStatus({required bool value}) =>
       box.write('all_azkar_finished_vibrate', value);
 }
