cdc5a58ad9b48f717fd3d865ba5f9f1fb7669cb3
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-16 19:59:13 +0300

    Update viewer.dart
    Calculation Logic

--- ruqayyah/lib/src/views/viewer.dart
+++ ruqayyah/lib/src/views/viewer.dart
@@ -2,7 +2,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:ruqayyah/src/models/rukia.dart';
-import 'package:ruqayyah/src/shared/functions/app_print.dart';
 import 'package:ruqayyah/src/utils/effect_manager.dart';
 
 class Viewer extends StatefulWidget {
@@ -29,43 +28,28 @@ class _ViewerState extends State<Viewer> {
     _pageController = PageController();
   }
 
-  Future<void> _onTap(Rukia item, int index) async {
-    if (item.count > 0) {
-      final count = item.count - 1;
-
-      if (count >= 0) {
-        rukiasToView[index] = item.copyWith(count: count < 0 ? 0 : count);
-        await EffectManager.onCount();
-      }
-
-      if (count == 0) {
-        await EffectManager.onSingleDone();
-        _pageController.nextPage(
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeInOut,
-        );
-      }
-    }
-
-    await checkProgress();
-    setState(() {});
-  }
-
+  int done = 0;
   double totalProgress = 0.0;
 
-  Future<void> checkProgress() async {
-    int done = 0;
-    for (var i = 0; i < rukiasToView.length; i++) {
-      if (rukiasToView[i].count == 0) {
-        done++;
-      }
-    }
-    totalProgress = done / (rukiasToView.length);
-    if (totalProgress == 1) {
-      await EffectManager.onAllDone();
+  Future<void> _onTap(int index) async {
+    final item = rukiasToView[index];
+
+    final count = item.count - 1;
+
+    if (item.count > 0 && count >= 0) {
+      rukiasToView[index] = item.copyWith(count: count < 0 ? 0 : count);
+      if (count == 0) done += 1;
+      await EffectManager.onCount();
     }
 
-    appPrint('$totalProgress = $done / ${rukiasToView.length}');
+    if (count <= 0) {
+      await EffectManager.onSingleDone();
+      _pageController.nextPage(
+        duration: const Duration(milliseconds: 300),
+        curve: Curves.easeInOut,
+      );
+    }
+
     setState(() {});
   }
 
@@ -78,7 +62,7 @@ class _ViewerState extends State<Viewer> {
         bottom: PreferredSize(
           preferredSize: const Size.fromHeight(10),
           child: LinearProgressIndicator(
-            value: totalProgress,
+            value: done / rukiasToView.length,
           ),
         ),
       ),
@@ -89,7 +73,7 @@ class _ViewerState extends State<Viewer> {
         itemBuilder: (context, index) {
           final item = rukiasToView[index];
           return InkWell(
-            onTap: () async => _onTap(item, index),
+            onTap: () async => _onTap(index),
             child: Padding(
               padding: const EdgeInsets.all(20),
               child: Stack(
