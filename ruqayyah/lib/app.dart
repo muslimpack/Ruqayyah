0fd48f0583c97b97b4da04efa46bd260d848fd2c
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-16 15:54:35 +0300

    Update app.dart

--- ruqayyah/lib/app.dart
+++ ruqayyah/lib/app.dart
@@ -1,4 +1,6 @@
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:ruqayyah/src/shared/functions/app_print.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -9,6 +11,14 @@ class App extends StatelessWidget {
       appBar: AppBar(),
       body: const Center(
         child: Text("Ruqayyah"),
+      ),
+      bottomNavigationBar: ConvexAppBar(
+        items: const [
+          TabItem(icon: Icons.short_text, title: 'موجزة'),
+          TabItem(icon: Icons.short_text, title: 'متوسطة'),
+          TabItem(icon: Icons.short_text, title: 'مطولة'),
+        ],
+        onTap: (int i) => appPrint('click index=$i'),
       ),
     );
   }
