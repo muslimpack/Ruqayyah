220a77a1be51f2c77477b418e684a3f78531c137
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2023-07-16 17:40:14 +0300

    Add Fonts

--- ruqayyah/lib/main.dart
+++ ruqayyah/lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
           seedColor: Colors.deepOrange,
           brightness: Brightness.dark,
         ),
+        fontFamily: "Uthmanic",
         useMaterial3: true,
       ),
       home: const App(),
