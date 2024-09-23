aef10d4acb95394973eed1eedb80a1752e7dcce2
7eltantawy <hasaneltantawy@gmail.com>
2024-09-24 02:34:19 +0300

    add font toolbox to settings screen

--- ruqayyah/lib/src/features/settings/presentation/screens/settings_screen.dart
+++ ruqayyah/lib/src/features/settings/presentation/screens/settings_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:ruqayyah/generated/l10n.dart';
 import 'package:ruqayyah/src/features/about/presentation/screens/about_screen.dart';
 import 'package:ruqayyah/src/features/effects_manager/presentation/screens/effects_manager_screen.dart';
+import 'package:ruqayyah/src/features/settings/presentation/components/font_settings_widgets.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
@@ -37,6 +39,9 @@ class _SettingsScreenState extends State<SettingsScreen> {
             },
           ),
           const Divider(),
+          Title(title: S.of(context).fontSettings),
+          const FontSettingsToolbox(),
+          const Divider(),
           ListTile(
             leading: const Icon(Icons.info_rounded),
             title: const Text(
@@ -54,6 +59,25 @@ class _SettingsScreenState extends State<SettingsScreen> {
             },
           ),
         ],
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  final String title;
+
+  const Title({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        title,
+        style: TextStyle(
+          fontSize: 20,
+          color: Theme.of(context).colorScheme.primary,
+        ),
       ),
     );
   }
