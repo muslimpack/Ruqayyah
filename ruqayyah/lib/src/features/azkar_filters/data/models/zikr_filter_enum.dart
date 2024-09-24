6caccdf45c817d5467923563368d5fe274c28d01
7eltantawy <hasaneltantawy@gmail.com>
2024-09-24 07:19:04 +0300

    Create zikr_filter_enum.dart

---
+++ ruqayyah/lib/src/features/azkar_filters/data/models/zikr_filter_enum.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:ruqayyah/generated/l10n.dart';
+
+enum ZikrFilter {
+  /// Source
+  quran,
+  sahihBukhari,
+  sahihMuslim,
+  abuDawood,
+  atTirmidhi,
+  anNasai,
+  ibnMajah,
+  malik,
+  adDarami,
+  ahmad,
+  ibnSunny,
+  hakim,
+  bayhaqi,
+  athar,
+
+  /// Hokm
+  hokmSahih,
+  hokmHasan,
+  hokmDaeif,
+  hokmMawdue,
+  hokmAthar,
+}
+
