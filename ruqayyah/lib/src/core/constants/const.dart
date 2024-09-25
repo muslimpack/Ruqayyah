89c02e8fe3f793e8b5886e4d2133575a249a56bd
7eltantawy <hasaneltantawy@gmail.com>
2024-09-25 11:09:42 +0300

    update app version

--- ruqayyah/lib/src/core/constants/const.dart
+++ ruqayyah/lib/src/core/constants/const.dart
@@ -1,11 +1,6 @@
 import 'package:ruqayyah/src/core/extensions/extension_platform.dart';
 
-final String kAppVersion = [
-  "0.3.1",
-  "0.3.0",
-  "0.2.0",
-  "0.1.0",
-].first;
+const String kAppVersion = "0.4.0";
 
 final String kAppStorageKey =
     PlatformExtension.isDesktop ? "ruqayyah_storage" : "GetStorage";
