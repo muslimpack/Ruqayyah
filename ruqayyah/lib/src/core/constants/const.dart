8ed60b0de9e00171a9e6c3b1cbfc3deb422c578d
Hassan Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-10-17 07:43:18 +0300

    Update app version

--- ruqayyah/lib/src/core/constants/const.dart
+++ ruqayyah/lib/src/core/constants/const.dart
@@ -1,7 +1,6 @@
 import 'package:ruqayyah/src/core/extensions/extension_platform.dart';
 
-const String kAppVersion = "0.4.0";
+const String kAppVersion = "0.4.01";
 
-final String kAppStorageKey =
-    PlatformExtension.isDesktop ? "ruqayyah_storage" : "GetStorage";
+final String kAppStorageKey = PlatformExtension.isDesktop ? "ruqayyah_storage" : "GetStorage";
 const String kAppName = "رقية";
