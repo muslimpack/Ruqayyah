a13973538d4eb4a1026310867ee080229d054c91
Hassan Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-11-22 20:38:03 +0200

    Update const.dart

--- ruqayyah/lib/src/core/constants/const.dart
+++ ruqayyah/lib/src/core/constants/const.dart
@@ -1,6 +1,6 @@
 import 'package:ruqayyah/src/core/extensions/extension_platform.dart';
 
-const String kAppVersion = "0.4.01";
+const String kAppVersion = "0.4.02";
 
 final String kAppStorageKey = PlatformExtension.isDesktop ? "ruqayyah_storage" : "GetStorage";
 const String kAppName = "رقية";
